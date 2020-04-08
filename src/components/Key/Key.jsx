467751cd7e51e010e2385bdb818c24cc6fccd242
shafali03 <shaf03sm@gmail.com>
2020-04-08 08:13:15 +0100

    test: Key.spec checks for the presence of the value of keyValue

--- src/components/Key/Key.jsx
+++ src/components/Key/Key.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Key = ({ keyAction, keyType, keyValue }) => <div className="key-container" />;
+const Key = ({ keyAction, keyType, keyValue }) => (
+  <div className="key-container">
+    <p className="key-value">
+      {keyValue}
+    </p>
+  </div>
+);
+
 
 Key.propTypes = {
   keyAction: PropTypes.func.isRequired,
